5e3e5a5949dcb0d7f194f0127aa316c9c1957437
alibeyI <alibey.ibrahimli@gmail.com>
2020-04-02 17:47:00 +0400

    Updates

---
+++ precache-manifest.bbfa7e11a06124012cc3ac8f73cf6faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d00eb4886df35317ae5195759c04474",
+    "url": "/Recipes_REACT/index.html"
+  },
+  {
+    "revision": "9d118f65a7b8d7349916",
+    "url": "/Recipes_REACT/static/css/main.f6dc4d21.chunk.css"
+  },
+  {
+    "revision": "86057c8d076b152a47b8",
+    "url": "/Recipes_REACT/static/js/2.b5fc2b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipes_REACT/static/js/2.b5fc2b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d118f65a7b8d7349916",
+    "url": "/Recipes_REACT/static/js/main.416b0fcc.chunk.js"
+  },
+  {
+    "revision": "1783cd5e01d152760ef9",
+    "url": "/Recipes_REACT/static/js/runtime-main.caddfd8d.js"
+  }
+]);
